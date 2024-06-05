27eb13fb8a7914c6325300edf40a13937573c0ee
AntonLysachev <anton@lysachev.ru>
2024-06-05 10:35:00 +0500

    degug

--- bot/webhooks.py
+++ bot/webhooks.py
@@ -40,10 +40,10 @@ async def process_sale_info(request):
 
 async def closed(object_id: int):
     receipt = await poster.get_receipt(object_id)
+    print('!!!!!!!!!!!!!!!!')
+    print(receipt)
     poster_id = receipt['client_id']
     chat_id = await get_chat_id_by_poster_id(poster_id)
-    print('!!!!!!!!!!!!!!!!')
-    print(chat_id)
     if chat_id:
         bot = Bot(token=TELEGRAM_TOKEN)
         buy_message = await get_buy_message(object_id)
